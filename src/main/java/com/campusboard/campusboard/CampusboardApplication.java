eee71d314c48046412d09e8db546f3407b0cd34f
Medo-com <ahmedsaid11213@gmail.com>
2025-12-10 13:46:22 -0700

    Initial commit

---
+++ src/main/java/com/campusboard/campusboard/CampusboardApplication.java
@@ -0,0 +1,13 @@
+package com.campusboard.campusboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusboardApplication.class, args);
+	}
+
+}
